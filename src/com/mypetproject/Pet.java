8a2131c9c1529de3a0ce0497802f0670e3a465ea
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-19 11:37:57 -0400

    Update Pet.java

--- src/com/mypetproject/Pet.java
+++ src/com/mypetproject/Pet.java
@@ -6,8 +6,8 @@ public class Pet {
     //constructor method:
     public Pet(String species) {
         this.species = species; 
-        //the first 'this.species' refers to the variable 'species' in THIS class, 
-        //the second 'species' refers to the parameter, whose value was entered when creating the new object. 
+        //the first 'this.species' refers to the variable 'species' in THIS class: String species = < enter string here >
+        //the second 'species' refers to the parameter, whose value was entered when creating the new object: Pet("cat").  
         //Ex: If we made an object like: Pet jeffrey = Pet("cat"), then in this class, the constructor would set the 'species' variable to 'cat', like: species = "cat" 
     }    
 
